eb8c5e091fd4cfe79e5247210c2a422d3440f3fa
Benjamin Dean <berman.reshetnikov@gmail.com>
2018-12-16 17:10:42 +0200

    Update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,34 +8,42 @@ class VibratingApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Vibration Plugin example app'),
-        ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              RaisedButton(
-                child: Text('Vibrate for default 500ms'),
-                onPressed: () {
-                  Vibration.vibrate();
-                },
-              ),
-              RaisedButton(
-                child: Text('Vibrate for 1000ms'),
-                onPressed: () {
-                  Vibration.vibrate(duration: 1000);
-                },
-              ),
-              RaisedButton(
-                child: Text('Vibrate with pattern 100v-200p-300v-400p-500v'),
-                onPressed: () {
-                  Vibration.vibrate(pattern: [100, 200, 300, 400, 500]);
-                },
-              )
-            ],
+          appBar: AppBar(
+            title: const Text('Vibration Plugin example app'),
           ),
-        ),
-      ),
+          body: Builder(builder: (BuildContext context) {
+            return Center(
+              child: Column(
+                children: <Widget>[
+                  RaisedButton(
+                    child: Text('Vibrate for default 500ms'),
+                    onPressed: () {
+                      Vibration.vibrate();
+                    },
+                  ),
+                  RaisedButton(
+                    child: Text('Vibrate for 1000ms'),
+                    onPressed: () {
+                      Vibration.vibrate(duration: 1000);
+                    },
+                  ),
+                  RaisedButton(
+                    child: Text('Vibrate with pattern'),
+                    onPressed: () {
+                      final snackBar = SnackBar(
+                        content: Text(
+                            'Pattern: wait 0.5s, vibrate 1s, wait 0.5s, vibrate 2s, wait 0.5s, vibrate 3s, wait 0.5s, vibrate 0.5s'),
+                      );
+
+                      Scaffold.of(context).showSnackBar(snackBar);
+                      Vibration.vibrate(
+                          pattern: [500, 1000, 500, 2000, 500, 3000, 500, 500]);
+                    },
+                  )
+                ],
+              ),
+            );
+          })),
     );
   }
 }
