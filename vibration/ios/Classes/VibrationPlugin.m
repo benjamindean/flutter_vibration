07857052eab8ecd068d512a15d737caf4689b79a
Steve <stevejobsadoptionist@gmail.com>
2020-11-11 01:52:33 +0800

    Fix compatiable with swift project. The generated compatibility header is not copied to header search path in swift project.

--- vibration/ios/Classes/VibrationPlugin.m
+++ vibration/ios/Classes/VibrationPlugin.m
@@ -1,5 +1,9 @@
 #import "VibrationPlugin.h"
+#if __has_include(<vibration/vibration-Swift.h>)
 #import <vibration/vibration-Swift.h>
+#else
+#import "vibration-Swift.h"
+#endif
 
 @implementation VibrationPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
