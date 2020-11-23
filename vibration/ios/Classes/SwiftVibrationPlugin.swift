82eb2e1e4910f3ebdbc22160935b017a9106c0a8
leicas <wspeed@gmail.com>
2020-11-23 13:38:19 -0500

    fixed typo

--- vibration/ios/Classes/SwiftVibrationPlugin.swift
+++ vibration/ios/Classes/SwiftVibrationPlugin.swift
@@ -106,7 +106,7 @@ public class SwiftVibrationPlugin: NSObject, FlutterPlugin {
 
             // Get event parameters, if any
             var params: [CHHapticEventParameter] = []
-            var amplitudes: [Int] = []
+            //var amplitudes: [Int] = []
             let amplitudes = myArgs["intensities"] as? [Int] 
             /*else
             {
